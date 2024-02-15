e18834862e25f68bcfba0663176f9b3aa496013a
shreyas-codes25 <140911851+shreyas-codes25@users.noreply.github.com>
2024-02-15 22:40:52 +0530

    basic build for trial

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -11,10 +11,47 @@ class LoginPage extends StatelessWidget{
           child:Center(
             child: Column(
               children:[
-                SizedBox(height: 100,),
-                Text("Hello world", style: TextStyle(fontSize: 50),),
-                SizedBox(height: 100,),
-                Icon(Icons.lock, size: 100,),
+                SizedBox(height: 50,),
+                Icon(Icons.lock, size: 50,),
+
+                SizedBox(height: 20,),
+                Text("Login", style: TextStyle(fontSize: 16,color:Colors.black)),
+
+                SizedBox(height: 20,),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                  child:  TextField(
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      border: OutlineInputBorder(),
+                      labelText: 'Username',
+                      hintText: 'Username',
+                    ),
+                  ),
+                ),
+
+                SizedBox(height: 20,),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 25.0),
+                child: TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    enabledBorder:OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Password',
+
+                  ),
+                ),
+             ),
+
+
+
+
               ]
             )
            ),
