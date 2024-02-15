3622c223e817e887a525a164d1e7f83d141fa01f
shreyas-codes25 <140911851+shreyas-codes25@users.noreply.github.com>
2024-02-15 23:24:15 +0530

    password eye button

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,63 +1,75 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget{
-  const LoginPage({super.key});
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
-    return const Scaffold(
+  LoginPageState createState() => LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage> {
+  bool passwordVisible = true;
+
+  void togglePasswordVisibility() {
+    setState(() {
+      passwordVisible = !passwordVisible;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       backgroundColor: Colors.orangeAccent,
-        body:SafeArea(
-          child:Center(
-            child: Column(
-              children:[
-                SizedBox(height: 50,),
-                Icon(Icons.lock, size: 50,),
-
-                SizedBox(height: 20,),
-                Text("Login", style: TextStyle(fontSize: 16,color:Colors.black)),
-
-                SizedBox(height: 20,),
-                Padding(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+              children: [
+                const SizedBox(height: 50,),
+                const Icon(Icons.lock, size: 50,),
+                const SizedBox(height: 20,),
+                const Text("Login", style: TextStyle(fontSize: 16,color:Colors.black)),
+                const SizedBox(height: 20,),
+                const Padding(
                   padding: EdgeInsets.symmetric(horizontal: 25.0),
-                  child:  TextField(
+                  child: TextField(
                     decoration: InputDecoration(
                       enabledBorder: OutlineInputBorder(
                         borderSide: BorderSide(color: Colors.white),
                       ),
                       border: OutlineInputBorder(),
-                      labelText: 'Username',
+
                       hintText: 'Username',
+                      fillColor: Colors.green,
+                      filled: true,
                     ),
                   ),
                 ),
-
-                SizedBox(height: 20,),
+               const SizedBox(height: 20,),
                 Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 25.0),
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    enabledBorder:OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                    ),
-                    border: OutlineInputBorder(),
-                    labelText: 'Password',
-                    hintText: 'Password',
+                  padding:const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: TextField(
+                    obscureText: passwordVisible,
+                    decoration: InputDecoration(
+                      suffixIcon: IconButton(
+                        onPressed: togglePasswordVisibility,
+                        icon: Icon(passwordVisible ? Icons.visibility : Icons.visibility_off),
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.white),
+                      ),
+                      fillColor: Colors.green,
+                      filled: true,
 
+                      hintText: 'Password',
+                    ),
                   ),
                 ),
-             ),
-
-
-
+                const SizedBox(height: 20,),
 
               ]
-            )
-           ),
-     )
-     );
-
-
+          ),
+        ),
+      ),
+    );
   }
 }
