dad8828f3f1c6ef71d26e8381bfa0f98a0d3794a
shreyas-codes25 <140911851+shreyas-codes25@users.noreply.github.com>
2024-04-13 00:43:05 +0530

    location finding done

--- lib/pages/petrol_pumps.dart
+++ lib/pages/petrol_pumps.dart
@@ -1,6 +1,5 @@
-
 import 'package:flutter/material.dart';
-
+import 'package:geolocator/geolocator.dart';
 
 class PetrolPumps extends StatefulWidget {
   const PetrolPumps({super.key});
@@ -10,6 +9,33 @@ class PetrolPumps extends StatefulWidget {
 }
 
 class _PetrolPumpsState extends State<PetrolPumps> {
+
+  Position? _currentPosition;
+  late bool servicePermission = false;
+  late LocationPermission permission;
+
+  String lat = "";
+  String long = "";
+
+  Future<Position> _getCurrentLocation() async {
+    servicePermission = await Geolocator.isLocationServiceEnabled();
+    if (!servicePermission) {
+      print("location service not enabled");
+
+    }
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+    }
+    return await Geolocator.getCurrentPosition();
+
+
+
+  }
+
+
+
+
   @override
   void initState() {
     super.initState();
@@ -19,21 +45,40 @@ class _PetrolPumpsState extends State<PetrolPumps> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body:
-        Container(
-           decoration: BoxDecoration(
-        gradient: LinearGradient(colors: [Colors.grey.shade900,Colors.grey.shade900],
-          begin: Alignment.topRight,
-          end: Alignment.bottomLeft,
-          stops: const [0.1,0.9],
+        body: Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+        colors: [Colors.grey.shade900, Colors.grey.shade900],
+        begin: Alignment.topRight,
+        end: Alignment.bottomLeft,
+        stops: const [0.1, 0.9],
+      )),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'Petrol Pumps near location: $lat, $long',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 40 * MediaQuery.of(context).size.aspectRatio),
+            ),
+            const SizedBox(
+              height: 7,
+            ),
 
-
-        )
-    ),
-          child: Center(
-              child: Text('Petrol Pumps',style: TextStyle(color: Colors.white,fontSize: 40 * MediaQuery.of(context).size.aspectRatio),)
-          ),
-        )
-    );
+            ElevatedButton(onPressed: () async {
+              _currentPosition = await _getCurrentLocation();
+              setState(() {
+                lat = _currentPosition!.latitude.toString();
+                long = _currentPosition!.longitude.toString();
+              });
+              print("$_currentPosition this is the address");
+            }, child: const Text("Get Location"))
+          ],
+        ),
+      ),
+    ));
   }
 }
